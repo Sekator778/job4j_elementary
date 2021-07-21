2fd4a1ab9ebd4d5cd27c5180d07bb1b6882f33f1
Sekator778 <sekator778@gmail.com>
2021-07-21 11:38:27 +0300

    4.6.2. Switch - matcher [#334071]

---
+++ test/ru/job4j/condition/MultipleSwitchWeekTest.java
@@ -0,0 +1,52 @@
+package ru.job4j.condition;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MultipleSwitchWeekTest {
+
+    @Test
+    public void whenMondayThen1() {
+        String day = "Monday";
+        int result = 1;
+        int expect = MultipleSwitchWeek.numberOfDay(day);
+
+        Assert.assertEquals(expect, result);
+    }
+
+    @Test
+    public void whenWednesdayThen3() {
+        String day = "Wednesday";
+        int result = 3;
+        int expect = MultipleSwitchWeek.numberOfDay(day);
+
+        Assert.assertEquals(expect, result);
+    }
+
+    @Test
+    public void whenWrongdayThen0() {
+        String day = "Wrongday";
+        int result = 0;
+        int expect = MultipleSwitchWeek.numberOfDay(day);
+
+        Assert.assertEquals(expect, result);
+    }
+
+    @Test
+    public void whenSybotaThen6() {
+        String day = "Суббота";
+        int result = 6;
+        int expect = MultipleSwitchWeek.numberOfDay(day);
+
+        Assert.assertEquals(expect, result);
+    }
+
+    @Test
+    public void whenVtornikThen2() {
+        String day = "Вторник";
+        int result = 2;
+        int expect = MultipleSwitchWeek.numberOfDay(day);
+
+        Assert.assertEquals(expect, result);
+    }
+}
